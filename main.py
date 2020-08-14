d2482b5dbd4c2c0a4291fc3a5b2ee7e8480cfefd
MARTIKRISO <mshkodrov@gmail.com>
2020-08-14 17:28:16 +0300

    Implemented login, main function

--- main.py
+++ main.py
@@ -10,18 +10,28 @@ db = mysql.connector.connect(
 cursor = db.cursor(buffered=True)
 
 
-def login():
+def login(uname, pswrd):
+    cursor.execute("SELECT * FROM Credentials WHERE username = %s", (uname, ))
+    row = cursor.fetchone()
+    passwordcorrect = False
     try:
-        pass
+        passwordcorrect = pswrd == row[1]
     except:
-        pass
+        print("Username does not exist in database!")
+        quit()
+    finally:
+        if passwordcorrect:
+            print("Login Successful!")
+        else:
+            print("Login Unsuccessful!")
+
 
 def register(uname, pswrd):
 
-    cursor.execute(f"SELECT EXISTS(SELECT * FROM Credentials WHERE username = \'{uname}\')")
+    cursor.execute("SELECT EXISTS(SELECT * FROM Credentials WHERE username = %s)", (uname, ))
     usernamefetch = cursor.fetchone()
     usernameexists = bool(usernamefetch[0])
-    cursor.execute(f"SELECT EXISTS(SELECT * FROM Credentials WHERE password = \'{pswrd}\')")
+    cursor.execute("SELECT EXISTS(SELECT * FROM Credentials WHERE password = %s)", (pswrd, ))
     passwordfetch = cursor.fetchone()
     passwordexists = bool(passwordfetch[0])
 
@@ -39,12 +49,28 @@ def register(uname, pswrd):
         db.commit()
         print("Registration Successful!")
 
-
+def showdata():
+    cursor.execute("SELECT * FROM Credentials ORDER BY id")
+    data = cursor.fetchall()
+    print(data)
 
 
 #cursor.execute
 #("CREATE TABLE Credentials(username VARCHAR(255), password VARCHAR(255), ID int PRIMARY KEY AUTO_INCREMENT)")
 
 if __name__ == "__main__":
-    register("testusername", "testpassword")
+    action = input("What action do you want to perform? \n1 - Login \n2 - Register \n")
+    username = input("Username: ")
+    password = input("Password: ")
+
+    if action == 1:
+        login(username, password)
+    elif action == 2:
+        register(username, password)
+    elif action == "admin":
+        showdata()
+    else:
+        print("That action is not valid!")
+
+
 
